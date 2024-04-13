e6336dd60e04ed84b108b50b2b082ace42f34fd7
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2024-04-13 15:31:18 +0900

    add: ゼミレポのヘッダー追加
    PRのコメントURL
    <https://github.com/YoslabEngineers/seminar-report-editor/pull/19#discussion_r1557041385>

--- next-app/src/app/edit/page.tsx
+++ next-app/src/app/edit/page.tsx
@@ -237,8 +237,60 @@ export default function Edit() {
                 <img />
               </div>
             </div>
+
+            {/* プレビュー本体 */}
             <div className='p-4'>
-              <p>1. はじめに</p>
+              {/* ゼミレポのヘッダー */}
+              <div className='flex justify-between items-center mb-4'>
+                <div>
+                  <p className='font-bold'>FACULTY OF SYSTEM ENGINEERING</p>
+                  <p className='font-bold'>WAKAYAMA UNIVERSITY</p>
+                </div>
+                <div>
+                  <p className='font-bold underline underline-offset-4'>No.100</p>
+                </div>
+              </div>
+
+              {/* タイトルとか */}
+              <div className='grid grid-rows-2 grid-cols-5 mb-6'>
+                <div className='relative h-16 col-span-3 border-2 border-black flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>TITLE</p>
+                  <p className='text-center text-2xl'>レポートタイトル</p>
+                </div>
+
+                <div className='relative h-16 col-span-1 border-2 border-black -ml-[1px] flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>REPORT No.</p>
+                  <p className='mr-2 text-right text-2xl'>123</p>
+                </div>
+
+                <div className='relative h-16 col-span-1 border-2 border-black -ml-[1px] flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>TOTAL</p>
+                  <p className='mr-2 text-right text-2xl'>1/1</p>
+                </div>
+
+                <div className='relative h-16 col-span-2 border-2 border-black -mt-[1px] flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>AUTHOR</p>
+                  <p className='text-center text-2xl'>Name</p>
+                </div>
+
+                <div className='relative h-16 col-span-1 border-2 border-black -ml-[1px] -mt-[1px] flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>POSITION</p>
+                  <p className='mr-2 text-right text-2xl'>Lab</p>
+                </div>
+
+                <div className='relative h-16 col-span-2 border-2 border-black -ml-[1px] -mt-[1px] flex flex-col justify-center'>
+                  <p className='absolute top-1 left-1'>DATE</p>
+                  <p className='text-center text-2xl'>2024年4月1日</p>
+                </div>
+              </div>
+
+              {/* レポートの本文 */}
+              <div>
+                <p className='text-2xl underline underline-offset-4'>1. はじめに</p>
+                <p>ああああああああああああああああああああ</p>
+                <p>ああああああああああああああああああああ</p>
+                <p>ああああああああああああああああああああ</p>
+              </div>
             </div>
           </div>
 
