1b43b6cfe60e64283fb34133561a7752c6e5b7be
taga <tagahei0505@gmail.com>
2024-05-03 16:59:04 +0900

    add: registerReportのテストの追加

---
+++ next-app/src/app/_application/service/__test__/registerReport.test.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect } from '@jest/globals'
+import { User } from '../../../_domain/model/user'
+import { Report } from '../../../_domain/model/report'
+import { ReportResource } from '../../../_domain/service/reportService'
+import { registerNewReport } from '../registerReport'
+
+jest.mock('../../../_domain/service/userService.ts', () => ({
+  getUser: (studentId: string) => {
+    return new User('Sample User Name', 'B3', studentId)
+  },
+}))
+
+jest.mock('../../../_domain/service/reportService.ts', () => ({
+  createReport: (resource: ReportResource) => {
+    if (resource.title.length > 30) {
+      return null
+    }
+
+    return new Report({
+      id: 1,
+      title: resource.title,
+      author: resource.author,
+      seminarDate: resource.seminarDate,
+      reportNumber: resource.reportNumber,
+      pageNumber: resource.pageNumber,
+      totalPages: resource.totalPages,
+      content: resource.content,
+      isSubmitted: resource.isSubmitted,
+    })
+  },
+}))
+
+describe('registerNewReport', () => {
+  it('新しいReportを登録に成功してtrueを返す', async () => {
+    const request = {
+      title: 'test title',
+      seminarDate: new Date(),
+      reportNumber: 1,
+      pageNumber: 1,
+      totalPages: 1,
+      content: '',
+      isSubmitted: false,
+      studentId: '12345678',
+    }
+
+    const result = await registerNewReport(request)
+
+    expect(result).toBe(true)
+  })
+
+  it('Reportが作成されない場合にfalseを返す', async () => {
+    const request = {
+      title: 'a'.repeat(31),
+      seminarDate: new Date(),
+      reportNumber: 1,
+      pageNumber: 1,
+      totalPages: 1,
+      content: '',
+      isSubmitted: false,
+      studentId: '12345678',
+    }
+
+    const result = await registerNewReport(request)
+
+    expect(result).toBe(false)
+  })
+})
