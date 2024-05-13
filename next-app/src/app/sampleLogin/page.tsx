2d3bb30d584809c0f66aabca93548a6be3c4064a
taga <tagahei0505@gmail.com>
2024-05-13 18:41:30 +0900

    add: サンプルログインページの作成

---
+++ next-app/src/app/sampleLogin/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+export default function Home(props: any) {
+  const { data } = useSession()
+  if (data) {
+    console.log(data)
+    return (
+      <>
+        ログインしています。 <br />
+        <button onClick={() => signOut()}>ログアウト</button>
+      </>
+    )
+  }
+
+  return (
+    <>
+      ログインしていません。 <br />
+      <button onClick={() => signIn()}>ログイン</button>
+    </>
+  )
+}
